13d31585221ada8762b2ae1da8c6fa744db5d6b7
Lucas Marques <lucas.rodrigues.caslu@gmail.com>
2025-08-13 12:24:34 -0300

    add useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import {Users2} from 'lucide-react'
 import UserItem from './components/userItem';
 
+import { useEffect,useState } from 'react';
+import { UserItemProps } from './types/user';
+
 function App (){
+
+  const [users,setUsers] = useState<UserItemProps[]>([])
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+      const response = await fetch("https://jsonplaceholder.typicode.com/users")
+
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getUsers
+  }, [])
   return(
     <div className="flex flex-col">
       <div className="flex items-center justify-center space-x-3 py-12"></div>
---
+++ src/types/user.ts
@@ -0,0 +1,11 @@
+export interface UserItemProps {
+
+    id: string;
+    name: string;
+    email: string;
+    phone: number;
+}
+
+export interface UserProps {
+    user: UserItemProps;
+}
